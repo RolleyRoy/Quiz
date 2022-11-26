e0de65e807451d96ad94adf2faff01148c64b00e
RolleyRoy <87445972+RolleyRoy@users.noreply.github.com>
2022-11-26 19:07:55 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,222 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "In CSS, h1 can be called as?",
+    answer: "Selector",
+    options: [
+      "Selector",
+       "Value",
+      "Attribute",
+       "Tag"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which HTML tag is used to define an internal style sheet?",
+    answer: "Style",
+    options: [
+      "CSS",
+      "Text/Style",
+      "Style",
+      "Script"
+    ]
+  },
+  {
+    numb: 8,
+    question: "CSS padding property is used for?",
+    answer: "Space",
+    options: [
+      "Border",
+      "Space",
+      "Margin",
+      "Background Color"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What are the three methods for using style sheets with a web page",
+    answer: "Inline, embedded or document level and external",
+    options: [
+        "Dreamweaver, GoLive or FrontPage",
+        "Handcoded, Generated or WYSIWYG",
+        "Inline, embedded or document level and external",
+        "text-decoration"
+    ]
+  },
+  {
+    numb: 10,
+    question: "In CSS, “font-size” can be called as?",
+    answer: "Property-Name",
+    options: [
+      "Selector",
+      "Rule",
+      "Property",
+      "Property-Name"
+    ]
+  },
+  {
+    numb: 11,
+    question: "How do you change the left margin of an element?",
+    answer: "Margin-left",
+    options: [
+      "left-margin",
+      "Margin-left",
+      "margin",
+      "text-indent"
+    ]
+  },
+  {
+    numb: 12,
+    question: "Which of the following methods is used to access HTML elements using Javascript?",
+    answer: "Both A and B",
+    options: [
+      "getElementbyId()",
+      "getElementByClassName()",
+      "Both A and B",
+      "only A"
+    ]
+  },
+  {
+    numb: 13,
+    question: "Which of the following methods can be used to display data in some form using Javascript?",
+    answer: "All of the above",
+    options: [
+      "document.write()",
+      "console.log()",
+      "window.alert()",
+      "All of the above"
+    ]
+  },
+  {
+    numb: 14,
+    question: "What is JavaScript?",
+    answer: "JavaScript is a compiled language used to make the website interactive",
+    options: [
+    "JavaScript is a scripting language used to make the website interactive",
+    "JavaScript is an assembly language used to make the website interactive",
+    "JavaScript is a compiled language used to make the website interactive",
+    "None of the mentioned"
+    ]
+  },
+  {
+    numb: 15,
+    question: "Arrays in JavaScript are defined by which of the following statements?",
+    answer: "It is an ordered list of values",
+    options: [
+      "It is an ordered list of values",
+      "It is an ordered list of objects",
+      "It is an ordered list of string",
+      "It is an ordered list of functions"
+    ]
+  },
+  {
+    numb: 16,
+    question: "Which of the following is not javascript data types?",
+    answer: " All of the mentioned",
+    options: [
+      "Null type",
+      "Undefined type",
+      " Number type",
+      " All of the mentioned"
+    ]
+  },
+  {
+    numb: 17,
+    question: "Which of the following object is the main entry point to all client-side JavaScript features and APIs?",
+    answer: "Window",
+    options: [
+      "Position",
+      "Window",
+      "Standard",
+      "Location"
+    ]
+  },
+  {
+    numb: 18,
+    question: "Which of the following can be used to call a JavaScript Code Snippet?",
+    answer: "Function/Method",
+    options: [
+      "Function/Method",
+      "Preprocessor",
+      "Triggering Event",
+      "RMI"
+    ]
+  },
+  {
+    numb: 19,
+    question: "What is the basic difference between JavaScript and Java?",
+    answer: "Functions are values, and there is no hard distinction between methods and fields",
+    options: [
+      "Functions are considered as fields",
+      "Functions are values, and there is no hard distinction between methods and fields",
+      "Variables are specific",
+      "There is no difference"
+    ]
+  },
+  {
+    numb: 20,
+    question: "Why event handlers is needed in JS?",
+    answer: "Allows JavaScript code to alter the behaviour of windows",
+    options: [
+      "Allows JavaScript code to alter the behaviour of windows",
+      "Adds innerHTML page to the code",
+      "Change the server location",
+      "Performs handling of exceptions and occurrences"
+    ]
+  },
+];
